b6782fd2da1479fbe7774988cc48f85d5af0f47c
ballMguy <supaluk29188@gmail.com>
2024-12-18 11:13:40 +0700

    test

---
+++ Question_2_2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Question_2_2 {
+    public static void main(String[] args) {
+        Scanner scanIn = new Scanner(System.in);
+        System.out.print("Enter N: ");
+        int n = scanIn.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                
+                if (i == 0 || i == n - 1 || j == 0 || j == n - 1)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
